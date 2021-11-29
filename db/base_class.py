191ea9f60e560caa2c4aaf635536becc58719ba7
Lucas Sossai <inv.lsossai@ifood3rd.com>
2021-11-29 14:43:51 -0300

    Added 2 base classes (Users and Job) and hashing method for password.

--- db/base_class.py
+++ db/base_class.py
@@ -1,11 +1,12 @@
 from typing import Any
-from sqlalchemy.ext.declarative import as_declarative,declared_attr
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
 @as_declarative()
 class Base:
-    id : Any
+    id: Any
     __name: str
 
-    def __tablename__(cls)->str:
+    @declared_attr
+    def __tablename__(cls) -> str:
         return cls.__name__.lower()
