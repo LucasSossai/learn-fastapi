b83fb96fc89973098fc351011547fc0ae68f4ec0
Lucas Sossai <inv.lsossai@ifood3rd.com>
2021-11-30 18:05:37 -0300

    added update and delete job endpoints

--- apis/v1/route_jobs.py
+++ apis/v1/route_jobs.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter, Depends
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from db.repository.jobs import create_new_job
+from db.repository.jobs import (
+    create_new_job,
+    delete_job_by_id,
+    retrieve_job,
+    list_jobs,
+    update_job_by_id,
+)
 from db.session import get_db
 from schemas.jobs import JobCreate, ShowJob
 
@@ -13,3 +20,46 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
     owner_id = 1
     job = create_new_job(job, db, owner_id)
     return job
+
+
+@router.get('/get/{id}', response_model=ShowJob)
+def retrieve_job_by_id(id: int, db: Session = Depends(get_db)):
+    job = retrieve_job(id=id, db=db)
+    print(job)
+    if job is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Job with id {id} does not exist',
+        )
+    return job
+
+
+@router.get('/all', response_model=List[ShowJob])
+def retrieve_all_jobs(db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    print(jobs)
+    return jobs
+
+
+@router.put('/update/{id}')
+def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Job with id {id} doest not exist',
+        )
+    return {'detail': 'Successfully updated the data'}
+
+
+@router.delete('/delete/{id}')
+def delete_job(id: int, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = delete_job_by_id(id=id, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Job with id {id} doest not exist',
+        )
+    return {'detail': f'Successfully deleted the job with id {id}'}
--- tests/test_routes/test_jobs.py
+++ tests/test_routes/test_jobs.py
@@ -4,16 +4,34 @@ from datetime import datetime
 
 def test_create_job(client):
     data = {
-        "title": "Job Opening Test",
-        "location": "Brazil",
-        "company": "Ashley Testing",
-        "description": "Company created for test",
+        'title': 'Job Opening Test',
+        'location': 'Brazil',
+        'company': 'Ashley Testing',
+        'description': 'Company created for test',
     }
-    response = client.post("/jobs/create-job", json.dumps(data))
+    response = client.post('/jobs/create-job', json.dumps(data))
     assert response.status_code == 200
-    assert response.json()["title"] == "Job Opening Test"
-    assert response.json()["location"] == "Brazil"
-    assert response.json()["company"] == "Ashley Testing"
-    assert response.json()["company_url"] is None
-    assert response.json()["description"] == "Company created for test"
-    assert response.json()["date_posted"] == str(datetime.now().date())
+    assert response.json()['title'] == 'Job Opening Test'
+    assert response.json()['location'] == 'Brazil'
+    assert response.json()['company'] == 'Ashley Testing'
+    assert response.json()['company_url'] is None
+    assert response.json()['description'] == 'Company created for test'
+    assert response.json()['date_posted'] == str(datetime.now().date())
+
+
+def test_retrieve_job_by_id(client):
+    data = {
+        'title': 'Job Opening Test',
+        'location': 'Brazil',
+        'company': 'Ashley Testing',
+        'description': 'Company created for test',
+    }
+    client.post('/jobs/create-job', json.dumps(data))
+    response = client.get('/jobs/get/1')
+    assert response.status_code == 200
+    assert response.json()['title'] == 'Job Opening Test'
+    assert response.json()['location'] == 'Brazil'
+    assert response.json()['company'] == 'Ashley Testing'
+    assert response.json()['company_url'] is None
+    assert response.json()['description'] == 'Company created for test'
+    assert response.json()['date_posted'] == str(datetime.now().date())
